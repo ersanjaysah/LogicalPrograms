c6ea52e4c8a95824ad0c8cae7c2576a78d4d8e91
sanjay <ssah6694@gmail.com>
2022-03-13 15:41:10 +0530

    [sanjay]Added: Problem4_Reverse a Number file

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P4_ReverseNumber
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int result = 0;
+            int remainder;
+            Console.WriteLine("enter your number");
+            int num = int.Parse(Console.ReadLine());
+
+            while (num!=0)
+            {
+                remainder=num % 10;
+                result = result*10 + remainder;
+                num = num / 10;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
