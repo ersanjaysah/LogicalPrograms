9f18b4828be3ee3ab850d20c01fd58b51795c6d9
sanjay <ssah6694@gmail.com>
2022-03-13 15:33:13 +0530

    [sanjay]Added: Problem2_Perfect Number file

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace P2_PerfectNumber
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int count = 0;
+            Console.WriteLine("enter a number");
+            int num = int.Parse(Console.ReadLine());
+           
+            for (int i = 1; i<num; i++)
+            {
+                
+
+                if(num%i==0)
+                {
+                     count=count+i;
+                }
+               
+
+            }
+            if (count == num)
+            {
+
+                Console.WriteLine("the number is perfect number "+count);
+            }
+            else
+            {
+                Console.WriteLine("the number is not a perfect number ");
+            }
+        }
+    }
+}
