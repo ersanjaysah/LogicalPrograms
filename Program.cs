2a4fa767b6c921eddf9e278fdff988540a5308dd
sanjay <ssah6694@gmail.com>
2022-03-13 15:37:18 +0530

    [sanjay]Added: Problem3_Prime Number file

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P3_PrimeNumber
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number");
+            int num = int.Parse(Console.ReadLine());
+            Program.MyMethod(num);
+        }
+
+        public static void MyMethod(int num)
+        {
+            int i = 2;
+
+            for (i = 2; i > 1; i++)
+            {
+
+                while (num % i == 0)
+                {
+                    Console.WriteLine("prime factors are= " + i);
+                    num = num / i;
+                }
+
+            }
+        }
+    }
+}
+
