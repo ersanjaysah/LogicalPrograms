d50e7d9b4fe22172d7400d9fab32abab476dd921
sanjay <ssah6694@gmail.com>
2022-03-13 15:44:35 +0530

    [sanjay]Added: Problem6_ Stop watch file

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace P6_StartAndStopWatch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("please enter your number");
+            int num = int.Parse(Console.ReadLine());
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine(i + " \n");
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Executiontime:{ stopwatch.ElapsedMilliseconds}sec");
+            Console.WriteLine("Time Elapsed is :{0}",stopwatch.Elapsed);
+            
+
+        }
+    }
+}
