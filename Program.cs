7259d9c4cc989b15d95b691bf3fca10282d2cd5f
sanjay <ssah6694@gmail.com>
2022-03-13 15:28:30 +0530

    [sanjay]Added: Problem1-Fibonacci series files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace P1_FibonacciSeries
+{
+    public class Program
+    {
+       public static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            for (int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
